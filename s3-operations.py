82afb37038e296ea86296785a07f0f0b00aebb7a
Traiano Welcome <traiano.welcome@teralytics.ch>
2018-03-31 22:04:06 +0800

    sample poc code for boto2

---
+++ s3-operations.py
@@ -0,0 +1,37 @@
+import boto
+import boto.s3.connection
+from boto.s3.key import Key
+
+'''Sample boto2 operations (FAST AND DIRTY. NOT SUITABLE PRODUCTION CODE): create a bucket, check permissions of access user, upload file to bucket, list files in bucket '''
+
+access_key = ''
+secret_key = ''
+conn = boto.connect_s3(
+aws_access_key_id = access_key,
+aws_secret_access_key = secret_key,
+host='s3.endpoint.aws.com',
+port=8067,
+is_secure=False,
+calling_format = boto.s3.connection.OrdinaryCallingFormat(),
+)
+bucket = conn.create_bucket('artifacts')
+
+for bucket in conn.get_all_buckets():
+        print "{name}\t{created}".format(
+                name = bucket.name,
+                created = bucket.creation_date,
+)
+
+acp = bucket.get_acl()
+for grant in acp.acl.grants:
+    print(grant.display_name, grant.permission)
+
+file_name='/tmp/data.gz'
+my_bucket = conn.lookup('artifacts')
+
+k=Key(bucket)
+k.key='testfile'
+k.set_contents_from_filename(file_name)
+
+for i in my_bucket.list():
+ print "beep: ",i
